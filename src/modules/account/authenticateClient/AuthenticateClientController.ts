dd63dccf14d24045ed1359f4641f26a64873980e
Gabriel Castellani <gabrielcastellanioliveira@gmail.com>
2022-09-12 19:27:36 -0300

    Create authenticate controller and authenticate use case

--- src/modules/account/authenticateClient/AuthenticateClientController.ts
+++ src/modules/account/authenticateClient/AuthenticateClientController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { CreateClientUseCase } from "./CreateClientUseCase";
+import { AuthenticateClientUseCase } from "./AuthenticateClientUseCase";
 
-export class CreateClientController {
+export class AuthenticateClientController {
     async handler(request: Request, response: Response) {
         const { username, password } = request.body;
 
-        const createClientUseCase = new CreateClientUseCase();
-        const result = await createClientUseCase.execute({
+        const authenticateClientUseCase = new AuthenticateClientUseCase();
+        const result = await authenticateClientUseCase.execute({
             username,
             password
         });
