9905300b81b61497913e2152c05b2f2acb3ff1fd
Gabriel Castellani <gabrielcastellanioliveira@gmail.com>
2022-09-12 19:15:56 -0300

    Create addUseCase to client

---
+++ src/modules/clients/useCases/createClient/CreateClientController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { CreateClientUseCase } from "./CreateClientUseCase";
+
+
+
+export class CreateClientController {
+    async handler(request: Request, response: Response) {
+        const { username, password } = request.body;
+
+        const createClientUseCase = new CreateClientUseCase();
+        const result = await createClientUseCase.execute({
+            username,
+            password
+        });
+
+        return response.json(result);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request: Request, response: Response) => {
-    return response.json({
-        message: "Hello Word"
-    })
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running"));
